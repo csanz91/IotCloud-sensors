502753441d9bc6debd2aa74c15669448835d59a0
Cesar Sanz <cesarsanz.91@gmail.com>
2022-11-26 06:52:16 +0100

    Add new servo sensor

---
+++ src/servo_button.cpp
@@ -0,0 +1,42 @@
+#include "servo_button.h" x
+
+SERVO_BUTTON::SERVO_BUTTON(
+    const char *sensor_id,
+    const char *sensor_name,
+    const char *metadata,
+    const unsigned long activation_time,
+    const unsigned long activation_angle,
+    const unsigned long resting_angle,
+    const int pin) : Toogle(sensor_id,
+                            sensor_name),
+                     _activation_time(activation_time),
+                     _activation_angle(activation_angle),
+                     _resting_angle(resting_angle)
+{
+    _servo = new Servo();
+    _servo->attach(pin);
+    push_button(false); // Go to the resting position
+}
+
+void SERVO_BUTTON::push_button(bool state)
+{
+    _servo->write(state ? _activation_angle : _resting_angle);
+}
+
+void SERVO_BUTTON::set_toogle_state(ToogleStates toogle_state)
+{
+    if (toogle_state == ToogleStates::TOOGLE)
+    {
+        push_button(true);
+        _last_toogled = millis();
+    }
+}
+
+void SERVO_BUTTON::loop()
+{
+    if (_last_toogled > 0 && millis() - _last_toogled > _activation_time)
+    {
+        push_button(false);
+        _last_toogled = 0;
+    }
+}
---
+++ src/servo_button.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <sensors/toogle.h>
+#include <Servo.h>
+
+class SERVO_BUTTON : public Toogle
+{
+
+private:
+    const long _activation_time;
+    const long _activation_angle;
+    const long _resting_angle;
+    unsigned long _last_toogled = 0;
+    Servo *_servo;
+    void push_button(bool state);
+    void set_toogle_state(ToogleStates);
+    void loop();
+
+public:
+    SERVO_BUTTON(
+        const char *sensor_id,
+        const char *sensor_name,
+        const char *metadata,
+        const unsigned long activation_time,
+        const unsigned long activation_angle,
+        const unsigned long resting_angle,
+        const int pin);
+};
